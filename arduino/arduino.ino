0279c01d3d31c25d811d306ef0792260629d73e8
Edwin Chen <e.chen1469@gmail.com>
2026-07-28 16:12:16 -0700

    Initialized variables for PID controller, servo, and velocity sensor Added setup code for initializing PID controller and servo Added PID controller loop Added placeholder code for velocity sensor loop Added comment blocks for better code readability

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,8 +1,12 @@
 #include <PID_v1.h> // PID library by Brett Beauregard
 #include "WiFiS3.h" // wifi lib
+#include <Servo.h> //Builtin servo library
 
-// define variables
-double setpoint, input, output;
+////////////////////////////////
+//                            //
+//    Networking Variables    //
+//                            //
+////////////////////////////////
 
 char ssid[] = "wheelchairbrake";
 char pass[] = "wheelchairbrake111";   
@@ -13,9 +17,74 @@ WiFiServer server(80);
 
 // ui components on arduino client
 
-void setup() {
-  // initialize PID variables
+////////////////////////////////
+//                            //
+//      Servo Variables       //
+//                            //
+////////////////////////////////
 
+Servo motor;
+
+const int pinServo = 9; //Position of servo's info pin
+int servoPos = 0; //Initialize the servo's initial position
+int servoWriteDelay = 15;
+
+////////////////////////////////
+//                            //
+// Velocity Sensor Variables  //
+//                            //
+////////////////////////////////
+
+const int pinHallEffect = 8; //Hall effect sensor's pin
+
+float velocityRot = 0; //Wheel's rotational velocity in rad/s
+float velocityTrans = 0; //PID's input; Translational velocity at this current instant in m/s.
+
+const float diameterWheel = 50; //Wheel's diameter
+volatile unsigned int pulseCount = 0; //The amount of times the hall effect sensor detects an input
+
+////////////////////////////////
+//                            //
+//        PID Variables       //
+//                            //
+////////////////////////////////
+
+float velocityTransMax = 1; //PID's setpoint; Max speed that the PID controller is trying to reach in m/s
+
+double Kp=2, Ki=5, Kd=1; //PID parameters; Proportion, integral, and derivative as scalars
+PID myPID(&velocityTrans, &servoPos, &velocityTransMax, Kp, Ki, Kd, DIRECT); //Initialize the PID controller
+
+void setup() {
+  ////////////////////////////////
+  //                            //
+  //        Servo Setup         //
+  //                            //
+  ////////////////////////////////
+  
+  motor.attach(pinServo); //Initializes the servo's pin
+  motor.write(0) //Forces the servo to the minimum position (THIS MIGHT BE REDUNDANT PLEASE VERIFY)
+
+  ////////////////////////////////
+  //                            //
+  //   Velocity Sensor Setup    //
+  //                            //
+  ////////////////////////////////
+  
+  //For the hall effect sensor
+
+  ////////////////////////////////
+  //                            //
+  //    PID Controller Setup    //
+  //                            //
+  ////////////////////////////////
+
+  myPID.SetMode(AUTOMATIC); //Turns the PID controller on
+
+  ////////////////////////////////
+  //                            //
+  //      Networking Setup      //
+  //                            //
+  ////////////////////////////////
 
   // Access Point setup
   Serial.begin(9600); // initialize serial comm
@@ -56,11 +125,16 @@ void setup() {
 
   printWiFiStatus();
 
-  // PID setup
-
 }
 
 void loop() {
+
+  ////////////////////////////////
+  //                            //
+  //      Networking Loop       //
+  //                            //
+  ////////////////////////////////
+
   // Check whether WiFi connection status changed
   if (status != WiFi.status()) {
     status = WiFi.status();
@@ -131,7 +205,34 @@ void loop() {
     Serial.println("Client disconnected");
   }
 
-  // PID Controls
+  ////////////////////////////////
+  //                            //
+  //         Servo Loop         //
+  //                            //
+  ////////////////////////////////
+
+  //This might be redundant because of the PID controller loop
+
+  ////////////////////////////////
+  //                            //
+  //    Velocity Sensor Loop    //
+  //                            //
+  ////////////////////////////////
+
+  //Read the wheel's pulse count and elapsed time
+  //Convert read data to rotational velocity
+  velocityTrans = velocityRot * (diameterWheel / 2);//Convert rotational velocity to translational velocity in m/s
+
+  ////////////////////////////////
+  //                            //
+  //    PID Controller Loop     //
+  //                            //
+  ////////////////////////////////
+
+  myPID.Compute(); //Compares the current translational velocity to the target maximum and queues a new servo rotation accordingly
+  analogWrite(pinServo, servoPos); //Send new servo rotation to servo
+  //This functionality might need a delay without pause (DO NOT USE delay() because it pauses the whole program)
+
 }
 
 void printWiFiStatus() {
